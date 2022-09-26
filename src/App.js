2679d9b0d2f2acd3b9f5b4c8c6c3103fe232b100
auschanh <auschanh@gmail.com>
2022-09-25 23:23:34 -0400

    first commit pt2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my personal site 🤨
+          This is my personal site... 🤨
         </p>
         <a
           className="App-link"
