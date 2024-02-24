0034d85f35c70b1ef6bce978c7f1961a4e9c7c7a
auschanh <auschanh@gmail.com>
2024-02-24 16:19:42 -0500

    navbar responsiveness fixes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // contact form msg received 
 const btn = document.getElementById('btn-contact');
 const msg = document.getElementById('msg');
+const aboutMore = document.getElementById('about-more');
 
 document.getElementById('form').addEventListener('submit', e => {
     msg.style.display = 'grid';
@@ -9,4 +10,15 @@ document.getElementById('form').addEventListener('submit', e => {
     }, 5000);
 });
 
+let show = false;
 
+document.getElementById('btn-more').addEventListener('click', e => {
+    show = !show;
+    if (show == true){
+        aboutMore.style.display = 'block';
+    }
+    else if (show == false) {
+        aboutMore.style.display = 'none';
+    }
+    
+});
