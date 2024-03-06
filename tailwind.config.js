eb746cf1601a9a0dc31906797130efef2c982c97
auschanh <auschanh@gmail.com>
2024-03-06 08:09:50 -0500

    Dark mode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {
-      
+      colors: {
+        'dark-navy': '#071121',
+        'navy': '#0B1C36'
+      }
     },
     container: {
       center: true,
