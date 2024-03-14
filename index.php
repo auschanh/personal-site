999c4b0ec4cc0e8cebc825db74abcaba05f01807
auschanh <auschanh@gmail.com>
2024-03-14 06:46:27 -0400

    animations added

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body>
@@ -44,8 +45,10 @@
 
 
     </div>
-    <script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
+    <script>
+    AOS.init();
     </script>
 </body>
 <script src="index.js"></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         firstName: { words: ["I'm Austin"], writeSpeed: 0.1 }, // 1s to type
         lastName: {
           words: ["CHANHSAVANG"],
-          writeSpeed: 0.1,
+          writeSpeed: 0.05,
           delay: 2,
           repeat: 0,
           eraseSpeed: 0,
@@ -34,10 +34,10 @@ module.exports = {
           words: [
             "I'm a software developer based in Toronto, with a passion for web development.",
           ],
-          writeSpeed: 0.05,
+          writeSpeed: 0.04,
           repeat: 0,
           eraseSpeed: 0,
-          delay: 2.5,
+          delay: 3.5,
         },
       },
     }),
