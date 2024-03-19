f2825075e442409f7f2203b96a2668c4349fe9c1
auschanh <auschanh@gmail.com>
2024-03-18 20:43:46 -0400

    mobile fixes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 " />
     <!-- Primary Meta Tags -->
     <meta name="title" content="Austin Chanhsavang - auschanh.com" />
     <meta name="description"
